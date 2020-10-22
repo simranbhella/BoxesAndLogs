435e3ea1ca9ca492b68cb97bbe9c90210907895f
simranbhella <71794401+simranbhella@users.noreply.github.com>
2020-10-22 17:57:03 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# BoxesAndLogs
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch1.js
@@ -0,0 +1,50 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var box1, box2, box3, box4, box5;
+var ground;
+var pig1, pig2;
+var log1, log2, log3, log4;
+
+function setup() {
+  createCanvas(1200,400);
+  engine = Engine.create();
+  world = engine.world;
+  ground=new Ground(600,height,1200,20);
+  box1= new Box(700,320,70,70);
+  box2=new Box(920,320,70,70);   
+  pig1=new Pig(810,350);
+  log1=new Log(810,260,20,PI);
+
+  box3= new Box(700,240,70,70);
+  box4=new Box(920,240,70,70);   
+  pig2=new Pig(810,220);
+  log2=new Log(810,180,20,PI);
+
+  box5 = new Box(810,160,70,70);
+  log3 = new Log(760,120,20,PI/4)
+  log4 = new Log(760,120,20,-PI/4)
+
+
+}
+
+function draw() {
+  background("black");
+
+  Engine.update(engine);
+  box1.display();
+  box2.display();
+  ground.display();
+  pig1.display();
+  log1.display();
+  box3.display();
+  box4.display();
+  pig2.display();
+  log2.display();
+  box5.display();
+  log3.display();
+  log4.display();
+  drawSprites();
+}
